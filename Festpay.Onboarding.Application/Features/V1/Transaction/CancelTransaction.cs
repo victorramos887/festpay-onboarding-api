97eed0f9cad643d1ab94606433d17d39f8960f94
Victor Ramos Pereira Mendes Soares <t_victor.psoares@hapvida.com.br>
2026-08-26 11:15:46 -0300

    feat: cancelled transaction endpoint

--- Festpay.Onboarding.Application/Features/V1/Transaction/CancelTransaction.cs
+++ Festpay.Onboarding.Application/Features/V1/Transaction/CancelTransaction.cs
@@ -1,4 +1,9 @@
+using Carter;
+using Festpay.Onboarding.Application.Common.Constants;
 using Festpay.Onboarding.Infra.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 
 public static class CancelTransaction
@@ -19,5 +24,28 @@ public static class CancelTransaction
         await dbContext.SaveChangesAsync(cancellationToken);
 
         return true;
+    }
+}
+
+public sealed class CancelTransactionCommandEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete(
+            $"{EndpointConstants.V1}{EndpointConstants.Transaction}/{{transactionId:guid}}",
+            async (
+                Guid transactionId,
+                FestpayContext dbContext,
+                CancellationToken cancellationToken) =>
+            {
+                var result = await CancelTransaction.Handle(
+                    dbContext,
+                    transactionId,
+                    cancellationToken);
+
+                return result ? Results.NoContent() : Results.NotFound();
+            }
+        )
+        .WithTags(SwaggerTagsConstants.Transaction);
     }
 }
